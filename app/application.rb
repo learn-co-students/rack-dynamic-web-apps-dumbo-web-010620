e3bfd64f97143d7d1aec6ba4c2b05e14285a4d09
github username <github email address>
2020-01-25 12:03:09 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,9 @@ class Application
 
   def call(env) # Basic Rack setup. def "call", then a Rack::Response.new initialization. with ___.finish to close out.
     resp = Rack::Response.new
-    num_1 = Kernel.rand(1..9)
-    num_2 = Kernel.rand(1..9)
-    num_3 = Kernel.rand(1..9)
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
 
     resp.write "#{num_1}, #{num_2}, #{num_3}\n"
 
